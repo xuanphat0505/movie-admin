0908e0b8c5053796038cbc744d3aaa7f00cc726d
phattran <phattran052004@gmail.com>
2026-06-29 20:33:17 +0700

    deploy with netlify

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true, // Bắt buộc phải tắt tối ưu hóa ảnh khi dùng static export
+  }
 };
 
 export default nextConfig;
